46776f5ab7a90321aceaa5cc203dab1e9b54fbb2
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-29 13:14:52 +0900

    Title: 시력 검사 2, Time: 225ms, Memory: 14MB, Status: Passed - CodeTree

--- 240329/시력 검사 2/eye-test-2.js
+++ 240329/시력 검사 2/eye-test-2.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 let input = Number(fs.readFileSync(0).toString());
 
 if (input >= 1.0) {
-  console.log('high');
+  console.log('High');
 } else if (input >= 0.5) {
   console.log('Middle');
 } else {
