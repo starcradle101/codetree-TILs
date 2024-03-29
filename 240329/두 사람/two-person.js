639fce5aa4b25c74dc3abb058dfe46e9986b9dd1
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-29 15:20:46 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240329/두 사람/two-person.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+let input = (fs.readFileSync(0).toString().split('\n'));
+
+let infoA = input[0].split(' ');
+let infoB = input[1].split(' ');
+
+let sexA = infoA[0].trim();
+let ageA = Number(infoA[1]);
+let sexB = infoB[0].trim();
+let ageB = Number(infoB[1]);
+
+if ((ageA >= 19 || ageB >= 19) && (sexA === 'M' || sexB === 'M')) {
+    console.log(1);
+} else {
+    console..log(0)
+}
