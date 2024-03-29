356ba354b69d9be74e46571654eeb1d4cd916812
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-29 17:57:24 +0900

    Title: 윤년인가, Time: 185ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240329/윤년인가/is-leap-year.js
+++ 240329/윤년인가/is-leap-year.js
@@ -2,3 +2,7 @@ const fs = require('fs');
 let input = Number(fs.readFileSync(0).toString());
 
 console.log((input % 4 === 0) || (input % 100 !== 0 && input % 400 === 0));
+
+// 4로 나누어 떨어지거나
+// 100으로 나누어 떨어지지 않고 400으로 나누어 떨어지는 해가 윤년
+// 둘 중 하나만 만족하면 되는 거 아닌가?
