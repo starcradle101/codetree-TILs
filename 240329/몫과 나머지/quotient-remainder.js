41291d5af25fc46db691408ede0c358a319ea045
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-29 10:57:25 +0900

    Title: 몫과 나머지, Time: 274ms, Memory: 15MB, Status: Passed - CodeTree

--- 240329/몫과 나머지/quotient-remainder.js
+++ 240329/몫과 나머지/quotient-remainder.js
@@ -4,7 +4,7 @@ let input = fs.readFileSync(0).toString().split(' ');
 let a = Number(input[0]);
 let b = Number(input[1]);
 
-let product = a * b;
+let quotient = parseInt(a / b);
 let remainder = a % b;
 
-console.log(`${product}...${remainder}`)
+console.log(`${quotient}...${remainder}`)
