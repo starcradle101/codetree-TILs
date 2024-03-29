86e7f4716de548031ad9effdeccfcaccdc9e9d19
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-29 10:23:58 +0900

    Title: 입력받은 값 교체하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240329/입력받은 값 교체하기/changing-inputs.js
+++ 240329/입력받은 값 교체하기/changing-inputs.js
@@ -4,6 +4,6 @@ let input = fs.readFilesync(0).toString().split(' ');
 let a = Number(input[0]);
 let b = Number(input[1]);
 
-let [a, b] = [b, a];
+[a, b] = [b, a];
 
-console.log(a, b);
+console.log(`${a} ${b}`);
