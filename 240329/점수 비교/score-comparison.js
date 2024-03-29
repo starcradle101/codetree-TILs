7a838509d2f99a4858663fa947a7f6d84c2f7031
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-29 14:38:54 +0900

    Title: 점수 비교, Time: 245ms, Memory: 14MB, Status: Passed - CodeTree

--- 240329/점수 비교/score-comparison.js
+++ 240329/점수 비교/score-comparison.js
@@ -4,10 +4,10 @@ let input = fs.readFileSync(0).toString().split('\n');
 let scoreA = input[0].split(' ');
 let scoreB = input[1].split(' ');
 
-let aMath = Number(scoreA[0][0]);
-let aEnglish = Number(scoreA[0][1]);
-let bMath = Number(scoreB[1][0]);
-let bEnglish = Number(scoreB[1][1]);
+let aMath = Number(scoreA[0]);
+let aEnglish = Number(scoreA[1]);
+let bMath = Number(scoreB[0]);
+let bEnglish = Number(scoreB[1]);
 
 if ( aMath > bMath && aEnglish > bEnglish) {
     console.log(1)
