ba6536ee2141ae7e058936d70243e1b5be12d268
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-29 10:33:15 +0900

    Title: 정수 세 개 입력받아 출력, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240329/정수 세 개 입력받아 출력/take-three-integers-and-output.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+let input = fs.readFilesync(0).toString().split('\n');
+
+let a = Number(input[0][0]);
+let b = Number(input[0][1]);
+let c = Number(input[1]);
+
+console.log(`${a} ${b} ${c}`);
