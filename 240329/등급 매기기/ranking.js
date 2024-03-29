fa45a810dc66d218a532176dfbd98a73f2f78257
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-29 13:18:54 +0900

    Title: 등급 매기기, Time: 233ms, Memory: 14MB, Status: Passed - CodeTree

--- 240329/등급 매기기/ranking.js
+++ 240329/등급 매기기/ranking.js
@@ -4,7 +4,7 @@ let input = Number(fs.readFileSync(0).toString());
 if (input >= 90) {
   console.log('A');
 } else if (input >= 80) {
-  console.log('Tom');
+  console.log('B');
 } else if (input >= 70) {
   console.log('C');
 } else if (input >= 60) {
