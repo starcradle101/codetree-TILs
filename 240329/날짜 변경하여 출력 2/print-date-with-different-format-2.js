c402a216425d5504cd23f13e4da045ac896a24a4
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-29 10:43:07 +0900

    Title: 날짜 변경하여 출력 2, Time: 225ms, Memory: 14MB, Status: Passed - CodeTree

--- 240329/날짜 변경하여 출력 2/print-date-with-different-format-2.js
+++ 240329/날짜 변경하여 출력 2/print-date-with-different-format-2.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 let input = fs.readFileSync(0).toString().split('-');
 
 let y = input[2];
-let m = input[1];
-let d = input[0]
+let m = input[0];
+let d = input[1]
 
 console.log(`${y}.${m}.${d}`);
