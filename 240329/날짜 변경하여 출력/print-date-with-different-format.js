443870c486f108fa7a0268d19ae1e6530cfdd208
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-29 10:44:55 +0900

    Title: 날짜 변경하여 출력, Time: 179ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240329/날짜 변경하여 출력/print-date-with-different-format.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().split('.');
+
+let year = input[0];
+let month = input[1];
+let day = input[2];
+
+console.log(`${year}-${month}-${day}`);
