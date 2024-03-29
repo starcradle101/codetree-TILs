2277d136a1a33d947faccc46f15882a2a42bae68
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-29 13:34:28 +0900

    Title: 특정 조건 두 정수 비교, Time: 181ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240329/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().split(' ');
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+let result1 = 0;
+let result2 = 0;
+
+if (a > b) {
+    result1 = 0
+} else {
+    result1 = 1
+}
+
+if (a === b) {
+    result2 = 1
+} else {
+    result2 = 0
+}
+
+console.log(result1, result2)
