8e8add8ac092d845809e43d8df19ddd5d2344cf8
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-29 13:24:54 +0900

    Title: 최대 2번의 연산, Time: 187ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240329/최대 2번의 연산/up-to-2-calculations.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+let input = Number(fs.readFileSync(0).toString());
+
+let a = input;
+
+if (a % 2 === 0) {
+    a /= 2;
+}
+
+if (a % 2===1) {
+    a+=1
+    a/=2
+}
+
+console.log(a)
