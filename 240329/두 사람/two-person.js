acf98640dd7e7db5c5b19e653ca4af1d3cc230c7
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-29 15:22:52 +0900

    Title: 두 사람, Time: 195ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240329/두 사람/two-person.js
+++ 240329/두 사람/two-person.js
@@ -4,10 +4,10 @@ let input = (fs.readFileSync(0).toString().split('\n'));
 let infoA = input[0].split(' ');
 let infoB = input[1].split(' ');
 
-let sexA = infoA[0].trim();
-let ageA = Number(infoA[1]);
-let sexB = infoB[0].trim();
-let ageB = Number(infoB[1]);
+let sexA = infoA[1].trim();
+let ageA = Number(infoA[0]);
+let sexB = infoB[1].trim();
+let ageB = Number(infoB[0]);
 
 if ((ageA >= 19 || ageB >= 19) && (sexA === 'M' || sexB === 'M')) {
     console.log(1);
