1ae6344ed4719dfb4afc9f9c348cc384cbe5a955
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-29 13:37:52 +0900

    Title: 비교 연산, Time: 245ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240329/비교 연산/comparison-operator.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().split(' ');
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+console.log(+(a >= b));
+console.log(+(a > b));
+console.log(+(b >= a));
+console.log(+(b > a));
+console.log(+(a === b));
+console.log(+(a !== b));
