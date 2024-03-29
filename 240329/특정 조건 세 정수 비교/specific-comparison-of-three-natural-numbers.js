5138ace6212b2301d12fdc80d2b8cf25f68a49ca
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-29 14:44:57 +0900

    Title: 특정 조건 세 정수 비교, Time: 289ms, Memory: 14MB, Status: Passed - CodeTree

--- 240329/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
+++ 240329/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -5,7 +5,7 @@ let a = Number(input[0]);
 let b = Number(input[1]);
 let c = Number(input[2]);
 
-let result1 = +(a < b && a < c);
+let result1 = +(a <= b && a <= c);
 let result2 = +(a === b && a === c && b === c);
 
 console.log(result1, result2)
