a5ecff08c330638f30d524314b7435101afbf1a3
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-30 19:31:18 +0900

    Title: 정수 입력받아 배수 출력, Time: 317ms, Memory: 14MB, Status: Passed - CodeTree

--- 240330/정수 입력받아 배수 출력/print-multiple-of-input.js
+++ 240330/정수 입력받아 배수 출력/print-multiple-of-input.js
@@ -4,7 +4,7 @@ let input = Number(fs.readFileSync(0).toString());
 let result = '';
 
 for (let i = input; i <= input * 5; i += input) {
-    result = i += ' ';
+    result += i + ' ';
 }
 
 console.log(result)
