575fb6bb07192b6baac235fbaad9a51340236fb2
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-30 20:35:26 +0900

    Title: 짝수만 출력 2, Time: 170ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 240330/짝수만 출력 2/output-only-even-2.js
+++ 240330/짝수만 출력 2/output-only-even-2.js
@@ -4,9 +4,9 @@ let input = (fs.readFileSync(0).toString().split(' '));
 let [b, a] = [Number(input[0]), Number(input[1])]
 let result = '';
 
-while (a <= b) {
-    result += a + ' '
-    a -= 2
+while (b <= a) {
+    result += b + ' '
+    b -= 2
 }
 
 console.log(result)
