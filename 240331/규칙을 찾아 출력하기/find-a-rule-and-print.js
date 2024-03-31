14fc355a915b2635ab2df502ea3b03178b8327cc
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-31 15:59:53 +0900

    Title: 규칙을 찾아 출력하기, Time: 225ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240331/규칙을 찾아 출력하기/find-a-rule-and-print.js
+++ 240331/규칙을 찾아 출력하기/find-a-rule-and-print.js
@@ -1,40 +1,24 @@
 const fs = require('fs');
 let n = Number((fs.readFileSync(0).toString()));
 
-let result = '';
+// 첫 번째 줄 출력
+console.log('* '.repeat(n).trim());
 
-for (let i = 0; i < n; i++) {
-    if (i === 0 || i === n - 1) {
-        result += '* '.repeat(n).trim() + '\n';
-    } else {
-        result += '* ' + '  '.repeat(n - 2) + '*\n';
+// 중간 줄 출력
+for (let i = 1; i < n - 1; i++) {
+    let line = '*';
+    for (let j = 1; j < n - 1; j++) {
+        if (i === j) {
+            line += ' ';
+        } else {
+            line += '*';
+        }
     }
+    line += ' *';
+    console.log(line);
 }
 
-console.log(result)
-
-// n = 3
-// * * *
-// *   *
-// * * *
-
-// n = 4
-// * * * *
-// *     *
-// * *   *
-// * * * *
-
-// n = 5
-// * * * * *
-// *       *
-// * *     *
-// * * *   *
-// * * * * *
-
-// n = 6
-// * * * * * *
-// *         *
-// * *       *
-// * * *     *
-// * * * *   *
-// * * * * * *
+// 마지막 줄 출력
+if (n > 1) {
+    console.log('* '.repeat(n).trim());
+}
