7d2ce7d930114544a8b1f996a428b3f66cf33fb6
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-31 16:51:21 +0900

    Title: 행에 따라 다르게 출력, Time: 162ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240331/행에 따라 다르게 출력/output-differently-for-row.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+let n = Number(fs.readFileSync(0).toString());
+
+let cnt = 1;
+let result = '';
+
+for (let i = 0; i < n; i += 1) {
+    for (let j = 0; j < n; j += 1) {
+        if (i % 2 === 0) {
+            result += cnt + ' ';
+            cnt += 1;
+        } else {
+            result += cnt + ' ';
+            cnt += 2;
+        }
+    }
+    result += '\n';
+}
+
+console.log(result)
