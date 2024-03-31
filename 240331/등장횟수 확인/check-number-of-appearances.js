9509542a2c7f08094f372e21cd983ac84df7a174
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-31 10:38:18 +0900

    Title: 등장횟수 확인, Time: 166ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240331/등장횟수 확인/check-number-of-appearances.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+let input = ((fs.readFileSync(0).toString().split('\n')));
+
+let result = 0;
+
+for (let i = 0; i < input.length; i += 1) {
+    if(Number(input[i]) % 2 === 0) {
+        result += 1;
+    }
+}
+
+console.log(result)
