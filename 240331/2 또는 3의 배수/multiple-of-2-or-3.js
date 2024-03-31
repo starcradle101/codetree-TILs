0c6273c7a2ee835dd84357a978e50fe6480b2ca2
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-31 10:29:44 +0900

    Title: 2 또는 3의 배수, Time: 231ms, Memory: 14MB, Status: Passed - CodeTree

--- 240331/2 또는 3의 배수/multiple-of-2-or-3.js
+++ 240331/2 또는 3의 배수/multiple-of-2-or-3.js
@@ -4,7 +4,7 @@ let input = Number((fs.readFileSync(0).toString()));
 let result = ''
 
 for (let i = 1; i <= input; i += 1) {
-    if (input % 3 === 0 || input % 2 === 0) {
+    if (i % 3 === 0 || i % 2 === 0) {
         result += 1 + ' ';
     } else {
         result += 0 + ' '
