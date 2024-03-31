69c1bb64ec69dbb755dfa1754206510e6310e3b8
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-31 11:55:57 +0900

    Title: 직각 삼각형 출력, Time: 174ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240331/직각 삼각형 출력/Right-triangle-and-output.js
+++ 240331/직각 삼각형 출력/Right-triangle-and-output.js
@@ -3,11 +3,8 @@ let input = Number(fs.readFileSync(0).toString());
 
 let triangle = '';
 
-for (let i = 0; i < input; i += 1) {
-    for (let j = 1; j <= (2 * j + 1); j += 1) {
-        triangle += '*'
-    }
-    triangle += '\n'
+for (let i = 1; i <= input; i += 1) {
+    triangle += "*".repeat(2 * i + 1) + '\n';
 }
 
 console.log(triangle)
