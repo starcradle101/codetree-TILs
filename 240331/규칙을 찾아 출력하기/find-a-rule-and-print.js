3852e1d4cef5ad7e9ac168adfd383a77efb4fc56
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-31 16:26:53 +0900

    Title: 규칙을 찾아 출력하기, Time: 170ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240331/규칙을 찾아 출력하기/find-a-rule-and-print.js
+++ 240331/규칙을 찾아 출력하기/find-a-rule-and-print.js
@@ -1,24 +1,47 @@
 const fs = require('fs');
 let n = Number((fs.readFileSync(0).toString()));
 
-// 첫 번째 줄 출력
-console.log('* '.repeat(n).trim());
+// 윗 줄과 밑 줄 ->  n개 출력 (1일때는 하나만)
 
-// 중간 줄 출력
-for (let i = 1; i < n - 1; i++) {
-    let line = '*';
-    for (let j = 1; j < n - 1; j++) {
-        if (i === j) {
-            line += ' ';
-        } else {
-            line += '*';
-        }
+let result = '';
+
+result += '* '.repeat(n) + '\n';
+
+// 요 사이에 빈칸 채울 것
+if (n >= 3) {
+    for (let i = 1; i < n - 1; i += 1) {
+        result += '* '.repeat(i) + ' '.repeat(i + 1) + '*\n';
     }
-    line += ' *';
-    console.log(line);
 }
 
-// 마지막 줄 출력
 if (n > 1) {
-    console.log('* '.repeat(n).trim());
+    result += '* '.repeat(n);
 }
+
+console.log(result)
+
+// 거꾸로 내려가는 모양
+// n = 3
+// 1
+
+// n = 4
+// 2
+// 1
+
+// n = 5
+// 3
+// 2
+// 1
+
+// n = 6
+// 4
+// 3
+// 2
+// 1
+
+// n = 7
+// 5
+// 4
+// 3
+// 2
+// 1
