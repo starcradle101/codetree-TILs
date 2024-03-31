2a292e18d2f45fcb8eccb049c84417a7ca4e5655
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-31 16:36:52 +0900

    Title: 열에 따라 감소하는 정사각형, Time: 183ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240331/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+let n = Number(fs.readFileSync(0).toString());
+
+let result = '';
+
+for (let i = 0; i < n; i += 1) {
+    for (let j = n; j >= 1; j -= 1) {
+        result += j + ' ';
+    }
+    result += '\n';
+}
+
+console.log(result)
