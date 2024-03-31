da15d0d422c4bfa3f1a1e8b9d23118e3a44f8233
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-03-31 10:39:13 +0900

    Title: 등장횟수 확인, Time: 198ms, Memory: 14MB, Status: Passed - CodeTree

--- 240331/등장횟수 확인/check-number-of-appearances.js
+++ 240331/등장횟수 확인/check-number-of-appearances.js
@@ -3,7 +3,7 @@ let input = ((fs.readFileSync(0).toString().split('\n')));
 
 let result = 0;
 
-for (let i = 0; i < input.length; i += 1) {
+for (let i = 0; i < 5; i += 1) {
     if(Number(input[i]) % 2 === 0) {
         result += 1;
     }
