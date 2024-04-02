f39af03474d0523a89a95cb935ba5517e0c85a7f
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-02 09:35:47 +0900

    Title: 알파벳 출력하기 2, Time: 206ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240402/알파벳 출력하기 2/print-alphabet-2.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+let n = fs.readFileSync(0).toString().trim();
+let result = '';
+let index = 'A'.charCodeAt();
+
+
+for (let i = 0; i < n; i += 1) {
+        result += '  '.repeat(i);
+    for (let j = i; j < n; j += 1) {
+        result += String.fromCharCode(index) + ' ';
+        index += 1;
+    }
+    result += '\n';
+}
+
+console.log(result)
+
+// n -> n 번 만큼 반복
+// 한 줄에 출력되는 알파벳 수 : n + 1 개 ... 1개까지 반복
+// 공백 : 0개 ... n - 1개
