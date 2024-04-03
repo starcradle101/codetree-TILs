db78b6ef2dec7901c9432d6eb11629a5114c61d0
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-03 10:27:17 +0900

    Title: 10개 입력받기, Time: 192ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240403/10개 입력받기/receive-10-inputs.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+let answer = Array(10);
+let sum = 0, cnt = 0;
+
+for (let el of input) {
+    if (el === 0) break;
+    sum += el;
+    cnt += 1;
+}
+
+let avg = (sum / cnt).toFixed(1);
+
+console.log(sum, avg);
