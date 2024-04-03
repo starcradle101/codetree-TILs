2b052b0462fa28c44ab2a3a0fe14c3d83c938b94
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-03 10:20:19 +0900

    Title: 학점 계산기, Time: 189ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240403/학점 계산기/credit-calculator.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+const n = Number(input[0]);
+const scores = input[1].split(' ').map(Number);
+
+let sum = 0, avg = 0;
+
+for (let score of scores) {
+    sum += score;
+}
+
+avg = (sum / (scores.length)).toFixed(1);
+
+if (avg >= 4.0) {
+    console.log('Perfect');
+} else if (avg >= 3.0) {
+    console.log('Good');
+} else {
+    console.log('Poor');
+}
