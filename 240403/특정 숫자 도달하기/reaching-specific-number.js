22536449b2d54bddca0340c521cdcf7086c99a45
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-03 10:14:14 +0900

    Title: 특정 숫자 도달하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240403/특정 숫자 도달하기/reaching-specific-number.js
+++ 240403/특정 숫자 도달하기/reaching-specific-number.js
@@ -1,18 +1,24 @@
 const fs = require('fs');
 let arr = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
-let sum = 0;
-let index = 0;
+let filteredArr = [];
+let [sum, avg] = [0, 0];
 
-for (let i = 0; i < arr.length; i += 1) {
-    if (arr[i] >= 250) {
-        index = i;
+for (let el of arr) {
+    if (el >= 250) {
         break;
     } else {
-        sum += arr[i];
+        filteredArr.push(el)
     }
 }
 
-let avg = (sum / (index)).toFixed(1);
+for (let el of filteredArr) {
+    sum += el;
+}
+
+let avg = (sum / filteredArr.length).toFixed(1);
 
 console.log(sum, avg)
+
+// 250 이상의 정수가 주어지면 마지막으로 주어진 수를 제외하고 주어진 모든 정수들의 합과 평균을 계산...
+// 만약 배열에 250 이상의 정수가 없다면 10개의 합계와 평균을 계산
