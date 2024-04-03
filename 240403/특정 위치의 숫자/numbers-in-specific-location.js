76975f4fa8611e3c62d2e0a22256f3c0fa2b5238
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-03 14:19:52 +0900

    Title: 특정 위치의 숫자, Time: 206ms, Memory: 14MB, Status: Passed - CodeTree

--- 240403/특정 위치의 숫자/numbers-in-specific-location.js
+++ 240403/특정 위치의 숫자/numbers-in-specific-location.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 let input = fs.readFileSync(0).toString().split(' ').map(Number);
 
 let sum = 0;
-for (let el of input) {
-    sum += el;
-}
+sum += input[2] + input[4] + input[9];
 
 console.log(sum)
