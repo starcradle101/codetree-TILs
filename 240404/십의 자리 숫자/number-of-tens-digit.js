d839c914df8a6dec72e61d0280cc6328e7d94953
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-04 18:38:39 +0900

    Title: 십의 자리 숫자, Time: 200ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240404/십의 자리 숫자/number-of-tens-digit.js
+++ 240404/십의 자리 숫자/number-of-tens-digit.js
@@ -20,9 +20,8 @@ for (let num of arrBefore0) {
 	}
 }
 
-console.log(arrDigit);
 
-for (let j = 1; j <= arrBefore0.length; j += 1) {
+for (let j = 1; j <= arrDigit.length; j += 1) {
 	result += `${j} - ${arrDigit[j]}\n`;
 }
 
