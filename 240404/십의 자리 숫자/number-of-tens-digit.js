e4e12b02eb27a790011d8b6611933cd759c1fa58
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-04 18:35:43 +0900

    Title: 십의 자리 숫자, Time: 194ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240404/십의 자리 숫자/number-of-tens-digit.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().split(' ').map(Number);
+
+let arrBefore0 = [];
+let result = '';
+
+for (let i = 0; i < input.length; i += 1) {
+	if (input[i] === 0) {
+		break;
+	}
+	arrBefore0.push(input[i]);
+}
+
+let arrDigit = Array(11).fill(0);
+
+for (let num of arrBefore0) {
+	let digit = parseInt(num / 10);
+	if (digit !== 0) {
+		arrDigit[digit] += 1;
+	}
+}
+
+for (let j = 1; j <= arrBefore0.length; j += 1) {
+	result += `${j} - ${arrDigit[j]}\n`;
+}
+
+console.log(result);
