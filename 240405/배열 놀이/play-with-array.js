ddb783588e09c5813d261dfdfe3d755f339258e1
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-05 23:44:40 +0900

    Title: 배열 놀이, Time: 205ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240405/배열 놀이/play-with-array.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().split('\n');
+
+let [elementNumber, queryNumber] = input[0].split(' ').map(Number);
+let elementArray = input[1].split(' ').map(Number);
+let queries = input.slice(2); // query 이차원 배열
+
+for (let i = 0; i < queryNumber; i += 1) {
+	let [queryCategory, queryElement] = queries[i].split(' ').map(Number);
+	console.log(queryCategory, queryElement);
+
+	switch (queryCategory) {
+		case 1:
+			console.log(elementArray[queryElement]);
+			break;
+		case 2:
+			let idx = 0;
+			for (let j = 0; j < elementNumber; j += 1) {}
+			break;
+		case 3:
+			let case3 = queries[i].split(' ').map(Number);
+			let [start, end] = [case3[1], case3[2]];
+			let result = '';
+
+			for (let j = start - 1; j < end; j += 1) {
+				result += elementArray[j] + ' ';
+			}
+			console.log(result);
+	}
+}
