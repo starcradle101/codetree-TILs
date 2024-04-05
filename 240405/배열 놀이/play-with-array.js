5f5147e240d9a1c6e478ce230ca0b44e68e8e687
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-05 23:48:27 +0900

    Title: 배열 놀이, Time: 241ms, Memory: 14MB, Status: Passed - CodeTree

--- 240405/배열 놀이/play-with-array.js
+++ 240405/배열 놀이/play-with-array.js
@@ -7,15 +7,26 @@ let queries = input.slice(2); // query 이차원 배열
 
 for (let i = 0; i < queryNumber; i += 1) {
 	let [queryCategory, queryElement] = queries[i].split(' ').map(Number);
-	console.log(queryCategory, queryElement);
 
 	switch (queryCategory) {
 		case 1:
-			console.log(elementArray[queryElement]);
+			console.log(elementArray[queryElement - 1]);
 			break;
 		case 2:
-			let idx = 0;
-			for (let j = 0; j < elementNumber; j += 1) {}
+			let idx = -1;
+			for (let j = 0; j < elementNumber; j += 1) {
+				if (elementArray[j] === queryElement) {
+					idx = j + 1;
+					break;
+				}
+			}
+
+			if (idx === -1) {
+				console.log(0);
+			} else {
+				console.log(idx);
+			}
+
 			break;
 		case 3:
 			let case3 = queries[i].split(' ').map(Number);
