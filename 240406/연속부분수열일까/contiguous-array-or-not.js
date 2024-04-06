a3dd8e5608a8644c1c722de3ff54b9c7e8e5cb86
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-06 15:39:10 +0900

    Title: 연속부분수열일까, Time: 172ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240406/연속부분수열일까/contiguous-array-or-not.js
+++ 240406/연속부분수열일까/contiguous-array-or-not.js
@@ -16,18 +16,30 @@ let flag = false;
 
 for (let i = 0; i < sequenceALength; i += 1) {
     if (sequenceA[i] === sequenceB[0]) {
+        let cnt = 0;
         for (let j = 0; j < sequenceBLength; j += 1) {
             if (sequenceA[i + j] === sequenceB[j]) {
-                flag = true;
-            } else {
-                flag = false;
-                break;
+                cnt += 1;
             }
         }
-        break;
+        if (cnt === sequenceB.length) {
+            flag = true;
+            break;
+        } else {
+            continue;
+        }
     }
 }
 
 // 바깥 for문은 다 돌지 않고 나가야 함. 언제? 모든 비교가 끝났을 때
+
+// 의사코드
+// 먼저 수열 A를 돌면서 A의 원소 중 B의 시작과 일치하는 부분이 있는지 확인
+// 일치한다면
+    // 수열 B를 돌면서 B 수열 전체가 일치하는지 확인
+    // 전체가 일치한다면 -> 여기서 바로 다이렉트로 빠져나가기
+    // 일치하지 않는다면 -> 바깥 for문 계속 돌기
+// 일치하지 않는다면
+    // false인 flag 반환
 
 console.log(flag ? 'Yes': 'No');
