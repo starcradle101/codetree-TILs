f1ea3f8452a93e420bfd060eca63e40a707624c4
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-06 15:54:09 +0900

    Title: 2가 3번째로 등장하는 위치, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240406/2가 3번째로 등장하는 위치/where-2-appears-3rd.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split('\n');
+
+const length = Number(input[0]);
+const arr = input[1].split(' ').map(Number);
+let accumulation = 0;
+let idx = -1
+
+for (let i = 0; i < length; i += 1) {
+    if (arr[i] === 2) accumulation += 1;
+    if (accumulation === 3) {
+        idx = i;
+        break;
+    }
+}
+
+
+console.log(i + 1);
