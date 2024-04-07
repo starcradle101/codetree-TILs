2e8dd64a695f72d6f99751aa9cdaa82e802a3cc8
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-07 21:43:18 +0900

    Title: n개의 숫자 중 최대 2개, Time: 183ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240407/n개의 숫자 중 최대 2개/two-max-of-n-num.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().split('\n');
+
+let length = Number(input[0]);
+let arr = input[1].split(' ').map(Number);
+
+// 가장 큰 수와 두 번째로 큰 수
+
+// 큰 수 = max로 찾으면 됨
+// 두 번째로 큰 수? = 흐음...
+
+// 배열에서 하나씩 내보내자
+
+arr.sort((a, b) => b - a);
+
+console.log(arr[0], arr[1]);
