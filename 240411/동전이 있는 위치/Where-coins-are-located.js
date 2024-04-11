ad554019f50579f85b398668916b7e381377c220
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-11 14:54:59 +0900

    Title: 동전이 있는 위치, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240411/동전이 있는 위치/Where-coins-are-located.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split('\n');
+
+let [size, coinNum] = input[0].split(' ').map(Number);
+let coins = input.slice(1).map(el => el.split(' ').map(Number));
+let arr2d = Array(size)
+	.fill(0)
+	.map(() => Array(size).fill(0));
+
+for (let i = 0; i < coinNum; i += 1) {
+    let coinposition = coins[i];
+    arr2d[coinposition[0] - 1][coinposition[1] - 1] = 1;
+}
+
+for (let i = 0; i < coinNum; i += 1) {
+    let result = '';
+    for (let j = 0; j < coinNum; j += 1) {
+        result += arr2d[i][j] + ' ';
+    }
+    console.log(result);
+}
