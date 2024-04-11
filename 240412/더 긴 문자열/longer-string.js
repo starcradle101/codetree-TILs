6b82d6194d3f94788e0ffff4f3e2b906ace78a2e
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-12 08:49:10 +0900

    Title: 더 긴 문자열, Time: 190ms, Memory: 14MB, Status: Passed - CodeTree

--- 240412/더 긴 문자열/longer-string.js
+++ 240412/더 긴 문자열/longer-string.js
@@ -5,9 +5,9 @@ let [length1, length2]  = input.map(el => el.length);
 
 
 if (length1 > length2) {
-    console.log(input[0])
+    console.log(input[0], length1)
 } else if (length1 < length2) {
-    console.log(input[1])
+    console.log(input[1], length2)
 } else {
     console.log('same')
 }
