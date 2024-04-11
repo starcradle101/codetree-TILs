84e9c547ef3a8382e955df0aa95cd494082fac02
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-11 14:06:00 +0900

    Title: 격자 반대로 채우기, Time: 196ms, Memory: 14MB, Status: Passed - CodeTree

--- 240411/격자 반대로 채우기/grid-anti-fill.js
+++ 240411/격자 반대로 채우기/grid-anti-fill.js
@@ -1,22 +1,24 @@
 const fs = require('fs');
 let n = Number(fs.readFileSync(0).toString().trim());
 let cnt = 1;
+let directionUp = true;
 let arr2d = Array(n)
 	.fill(0)
 	.map(() => Array(n).fill(0));
 
-// n -1 부터 시작해서 -2씩 갈 때는 아래에서 위로
-// n -2부터 시작해서 -2씩 갈 때는 위에서 아래로
 
-for (let i = n - 1; i >= 0; i -= 2) {
-    if (i % 2 === 1) {
+for (let i = n - 1; i >= 0; i -= 1) {
+    
+    if (directionUp) {
         for (let j = n - 1; j >= 0; j -= 1) {
             arr2d[j][i] = cnt++;
         }
+        directionUp = false;
     } else {
         for(let j = 0; j < n; j += 1) {
             arr2d[j][i] = cnt++;
         }
+        directionUp = true;
     }
     
 }
