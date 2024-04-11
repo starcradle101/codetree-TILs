1885204f2f2ba0eaf7101845dcf1e975fe109c85
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-11 14:29:24 +0900

    Title: 파스칼의 삼각형, Time: 178ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 240411/파스칼의 삼각형/pascal's-triangle.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+let n = Number(fs.readFileSync(0).toString().trim());
+let arr2d = Array(n)
+	.fill(0)
+	.map(() => Array(n).fill(0));
+
+for (let i = 0; i < n; i += 1) {
+	arr2d[i][0] = 1;
+}
+
+for (let i = 1; i < n; i += 1) {
+	for (let j = 1; j < i + 1; j += 1) {
+		arr2d[i][j] = arr2d[i - 1][j - 1] + arr2d[i - 1][j];
+	}
+}
+
+for (let i = 0; i < n; i += 1) {
+	let result = '';
+	for (let j = 0; j < n; j += 1) {
+		if (arr2d[i][j] === 0) break;
+		result += arr2d[i][j] + ' ';
+	}
+	console.log(result);
+}
