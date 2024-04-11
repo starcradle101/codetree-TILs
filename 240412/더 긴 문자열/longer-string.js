4d557e75c3851ce93dd1baf74ec82f1c277843d7
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-12 08:48:51 +0900

    Title: 더 긴 문자열, Time: 192ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240412/더 긴 문자열/longer-string.js
+++ 240412/더 긴 문자열/longer-string.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
-let input = fs.readFileSync(0).toString().trim().split('\n');
+let input = fs.readFileSync(0).toString().trim().split(' ');
 
 let [length1, length2]  = input.map(el => el.length);
+
 
 if (length1 > length2) {
     console.log(input[0])
