129a9884a135d93f0afcfb1deef87a57f33f1899
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-12 14:06:07 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 193ms, Memory: 21MB, Status: Passed - CodeTree

--- 240412/특정 문자로 끝나는 문자열/string-ending-with-specific-character.js
+++ 240412/특정 문자로 끝나는 문자열/string-ending-with-specific-character.js
@@ -3,11 +3,15 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const words = input.slice(0, 10);
 const queryChar = input[10];
+let result = '';
 
 for (let i = 0; i < words.length; i += 1) {
     let endOfWord = words[i][words[i].length - 1];
 
     if (endOfWord === queryChar) {
-        console.log(words[i]);
+        result += words[i] + '\n';
     }
 }
+
+
+(result.length === 0) ? console.log('None') : console.log(result);
