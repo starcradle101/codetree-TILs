92cd9bd4422000f8e18a97e8748f69027e409699
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-13 22:20:06 +0900

    Title: Run Length 인코딩, Time: 194ms, Memory: 15MB, Status: Passed - CodeTree

--- 240413/Run Length 인코딩/run-length-encoding.js
+++ 240413/Run Length 인코딩/run-length-encoding.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 let word = fs.readFileSync(0).toString().trim();
-let [char, count] = [word[0],0];
+let [char, count] = [word[0],1];
 let compressedString = '';
 
-for (let i = 0; i < word.lenth; i += 1) {
-    if (word[i] !== word[i - 1]) {
+for (let i = 1; i <= word.length; i += 1) {
+    if (word[i - 1] !== word[i]) {
         compressedString += char + count;
-        [char, count] = [word[i], 0];
+        [char, count] = [word[i], 1];
     } else {
         count += 1
     }
