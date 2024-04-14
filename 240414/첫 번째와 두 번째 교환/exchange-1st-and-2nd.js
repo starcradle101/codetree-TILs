76694a420ff2b9c39ae101fbf7b98311c5110803
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-14 16:22:00 +0900

    Title: 첫 번째와 두 번째 교환, Time: 175ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240414/첫 번째와 두 번째 교환/exchange-1st-and-2nd.js
+++ 240414/첫 번째와 두 번째 교환/exchange-1st-and-2nd.js
@@ -3,11 +3,10 @@ let input = fs.readFileSync(0).toString().trim().split('');
 
 let [firstChar, secondChar] = input.slice(0, 2);
 
-for (let i = 0; i < input.length; i += 1) {
+for (let i = 0; i < input.length - 1; i += 1) {
     if (input[i] === firstChar) {
         input[i] = secondChar;
-    }
-    if (input[i]  === secondChar) {
+    } else if (input[i] === secondChar) {
         input[i] = firstChar;
     }
 }
