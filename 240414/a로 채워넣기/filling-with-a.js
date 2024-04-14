0723936817dc010ab1512f6e7ceffe9a5d35bdcf
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-14 16:12:55 +0900

    Title: a로 채워넣기, Time: 183ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240414/a로 채워넣기/filling-with-a.js
+++ 240414/a로 채워넣기/filling-with-a.js
@@ -4,7 +4,7 @@ let input = fs.readFileSync(0).toString().trim().split(' ');
 let length = input.length;
 
 input[1] = 'a';
-input[length - 2] = 'a';
+input[length - 3] = 'a';
 
 console.log(input.join(''));
 // 앞에서 두번째 뒤에서 두 번째
