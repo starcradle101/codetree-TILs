09fa629c9dba4db63e5b54dd994a7143313bc062
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-14 16:54:23 +0900

    Title: 두 번째를 첫 번째로, Time: 202ms, Memory: 14MB, Status: Passed - CodeTree

--- 240414/두 번째를 첫 번째로/second-to-first.js
+++ 240414/두 번째를 첫 번째로/second-to-first.js
@@ -4,8 +4,8 @@ let input = fs.readFileSync(0).toString().trim().split('');
 let [first, second] = input.slice(0, 2);
 
 for (let i = 0; i < input.length; i += 1) {
-    if (input[i] === first) {
-        input[i] = second;
+    if (input[i] === second) {
+        input[i] = first;
     }
 }
 
