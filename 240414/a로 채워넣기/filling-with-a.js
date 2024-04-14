f88d39e1f7cb6f54513e431f3e8070e1b158475f
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-14 16:12:32 +0900

    Title: a로 채워넣기, Time: 189ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240414/a로 채워넣기/filling-with-a.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(' ');
+
+let length = input.length;
+
+input[1] = 'a';
+input[length - 2] = 'a';
+
+console.log(input.join(''));
+// 앞에서 두번째 뒤에서 두 번째
