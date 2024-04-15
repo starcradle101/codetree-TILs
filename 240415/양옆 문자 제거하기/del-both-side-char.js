3f0634fae4a38d865a3670b5ad3390d86186a8b3
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-15 17:54:57 +0900

    Title: 양옆 문자 제거하기, Time: 186ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240415/양옆 문자 제거하기/del-both-side-char.js
+++ 240415/양옆 문자 제거하기/del-both-side-char.js
@@ -4,5 +4,5 @@ let input = fs.readFileSync(0).toString().trim();
 // slice 활용해서 자르기
 // 앞에서 2번째 원소, 뒤에서 2번째 원소 제거
 
-let answer = input.slice(0, 1) + input.slice(2, input.length - 4) + input.slice(input.length - 3);
+let answer = input.slice(0, 1) + input.slice(2, input.length - 3) + input.slice(input.length - 1);
 console.log(answer)
