384e0233cecbc6750c6a083be158c9a8a45eb360
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-16 14:30:10 +0900

    Title: 문자열 돌리기, Time: 231ms, Memory: 14MB, Status: Passed - CodeTree

--- 240416/문자열 돌리기/SPin-SPring.js
+++ 240416/문자열 돌리기/SPin-SPring.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 let string = fs.readFileSync(0).toString().trim();
+console.log(string);
 
 for (let i = 0; i < string.length; i += 1) {
-    string = string.slice(-1) + string.slice(1);
+    string = string.slice(-1) + string.slice(0, -1);
     console.log(string);
 }
