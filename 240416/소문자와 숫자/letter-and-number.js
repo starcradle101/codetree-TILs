cdc7392e56107b9f43445f987ecbbca6263fe91c
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-16 15:45:09 +0900

    Title: 소문자와 숫자, Time: 171ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240416/소문자와 숫자/letter-and-number.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim();
+let result = '';
+
+for (let i = 0; i < input.length; i += 1) {
+    if (input[i] <= 'Z' && input[i] >= 'A') {
+        result += input[i].toLowerCase();
+    } else if (input[i] >= 'a' && input[i] <= 'z') {
+        result += input[i];
+    }
+}
+
+console.log(result);
+
+// 알파벳만 대문자로
