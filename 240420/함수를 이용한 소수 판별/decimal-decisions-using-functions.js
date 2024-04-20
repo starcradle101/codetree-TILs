2dfd5ddcacfe5d7d9493ccdbeadf9e2bf1d0c47a
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-20 10:57:41 +0900

    Title: 함수를 이용한 소수 판별, Time: 172ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240420/함수를 이용한 소수 판별/decimal-decisions-using-functions.js
@@ -0,0 +1,26 @@
+// 함수를 이용한 소수 판별
+// a 이상 b 이하 소수들의 합
+
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+let [a, b] = input;
+
+// 소수의 합 구하는 함수
+function getPrimeSum(a, b) {
+    let primeSum = 0;
+ for (let i = a; i <= b; i += 1) {
+    if (findPrime(i)) primeSum += i;
+ }
+ return primeSum;
+}
+
+// 소수를 판별하는 함수
+function findPrime(n) {
+    for (let i = 2; i < n; i +=1 ) {
+        if (n % i === 0) return false;
+    }
+    return true;
+}
+
+let result = getPrimeSum(a, b);
+console.log(result)
