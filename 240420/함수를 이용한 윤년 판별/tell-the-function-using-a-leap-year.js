8d982da2248f406b0ca0b2ba4dd1461f9340fbaa
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-20 11:19:22 +0900

    Title: 함수를 이용한 윤년 판별, Time: 236ms, Memory: 26MB, Status: Passed - CodeTree

--- 240420/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.js
+++ 240420/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.js
@@ -2,10 +2,7 @@ const fs = require('fs');
 const year = Number(fs.readFileSync(0).toString().trim());
 
 const isLeapYear = (year) => {
-    if (year % 4 === 0 || year % 100 !== 0 && year % 400 === 0) {
-        return true;
-    }
-    return false;
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
 }
 
 let result = isLeapYear(year);
