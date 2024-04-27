03d890c23df7d529a6cdfe3be53516bd2d8a8d80
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-27 23:32:52 +0900

    Title: 2021년 날짜의 유무, Time: 174ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240427/2021년 날짜의 유무/with-or-without-2021.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const [month, day] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+
+// M월 D일, 존재한다면 Yes, 아니라면 No
+
+function isCorrectDate(month, day) {
+    const daysPerMonth = [
+        31, // January
+        28,
+        31, // March
+        30, // April
+        31, // May
+        30, // June
+        31, // July
+        31, // August
+        30, // September
+        31, // October
+        30, // November
+        31  // December
+    ];
+    if (daysPerMonth[month - 1] < day) return 'No';
+
+    return 'Yes';
+}
+
+let answer = isCorrectDate(month, day);
+console.log(answer)
+
+
+//
