c3f6a702036b39215acef137a54b01c1bb90a25c
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-27 23:11:49 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 263ms, Memory: 47MB, Status: Passed - CodeTree

--- 240427/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.js
+++ 240427/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.js
@@ -6,23 +6,24 @@ const seriesA = input[1].split(' ').map(Number);
 const seriesB = input[2].split(' ').map(Number);
 
 function isConsecutiveNumberSubSequence(seriesA, seriesB, sizeA, sizeB) {
-	let answer = 'No';
+    let answer = 'No';
 
-	for (let i = 0; i < sizeA; i += 1) {
-		if (seriesA[i] === seriesB[0]) {
-			if (sizeA - seriesA[i] - 1 < sizeB) {
-				break;
-			} else {
-				let index = seriesA[i];
-				for (let j = 0; j < sizeB; j += 1) {
-					if (seriesA[index + j] !== seriesB[j]) break;
-				}
-				answer = 'Yes';
-				break;
-			}
-		}
-	}
-	return answer;
+    for (let i = 0; i <= sizeA - sizeB; i++) {
+        if (seriesA[i] === seriesB[0]) {
+            let isSubsequence = true;
+            for (let j = 0; j < sizeB; j++) {
+                if (seriesA[i + j] !== seriesB[j]) {
+                    isSubsequence = false;
+                    break;
+                }
+            }
+            if (isSubsequence) {
+                answer = 'Yes';
+                break;
+            }
+        }
+    }
+    return answer;
 }
 
 let result = isConsecutiveNumberSubSequence(seriesA, seriesB, sizeA, sizeB);
