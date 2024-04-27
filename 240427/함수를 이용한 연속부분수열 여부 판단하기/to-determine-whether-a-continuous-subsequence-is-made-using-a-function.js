3d0229e3a8918b81c747410519d9b5f4d7a69e69
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-04-27 23:11:02 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 247ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240427/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.js
+++ 240427/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.js
@@ -5,32 +5,25 @@ const [sizeA, sizeB] = input[0].split(' ').map(Number);
 const seriesA = input[1].split(' ').map(Number);
 const seriesB = input[2].split(' ').map(Number);
 
-// B가 A의 원소들을 연속하게 뽑았을 때 나올 수 있는 수열이라면 연속부분 수열
-
 function isConsecutiveNumberSubSequence(seriesA, seriesB, sizeA, sizeB) {
-    let answer = 'No';
-    // B와 일치하는 수열의 시작을 A에서 찾았을 때
-    // 남은 길이를 먼저 확인해서 길이가 충분하지 않다면 NO
-    // 길이가 일치한다면 배열을 끝까지 돌면서
-    // break 없이 돌았다면
-    // 연속부분수열이다
+	let answer = 'No';
 
-    for (let i = 0; i < sizeA; i += 1) {
-        if (seriesA[i] === seriesB[0]) {
-            if (sizeA - seriesA[i] - 1 < sizeB) {
-                break;
-            } else {
-                let index = seriesA[i]
-                for (let j = 0; j < sizeB; j += 1) {
-                    if (seriesA[index + j] !== seriesB[j]) break;
-                }
-                answer = 'Yes';
-            }
-            
-        }
-    }
-    return answer;
+	for (let i = 0; i < sizeA; i += 1) {
+		if (seriesA[i] === seriesB[0]) {
+			if (sizeA - seriesA[i] - 1 < sizeB) {
+				break;
+			} else {
+				let index = seriesA[i];
+				for (let j = 0; j < sizeB; j += 1) {
+					if (seriesA[index + j] !== seriesB[j]) break;
+				}
+				answer = 'Yes';
+				break;
+			}
+		}
+	}
+	return answer;
 }
 
-let result = isConsecutiveNumberSubSequence(seriesA,seriesB,sizeA,sizeB);
-console.log(result)
+let result = isConsecutiveNumberSubSequence(seriesA, seriesB, sizeA, sizeB);
+console.log(result);
