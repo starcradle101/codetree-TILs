ee4b73a1228fc207f87c871cf559d2cc67494f88
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-05-17 18:20:55 +0900

    Title: 반복 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240517/반복 출력하기/repeated-output.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+let input = Number(fs.readFileSync(0).toString.trim());
+
+const printString = (time) => {
+    for (let i = 0; i < time; i += 1) {
+        console.log('12345^&*()_');
+    }
+}
+
+printString(input);
