3998ecd8c7e46564d55ef5f8a594e5ac6566007b
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-05-20 12:49:04 +0900

    Title: 절댓값으로 변경, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240520/절댓값으로 변경/find-the-absolute-value.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const arr = input[1].split(' ').map(Number);
+
+function makeAbs(arr) {
+    let result = arr.map(el => Math.abs(el));
+}
+
+let result = makeAbs(arr).join(' ');
+
+console.log(result)
