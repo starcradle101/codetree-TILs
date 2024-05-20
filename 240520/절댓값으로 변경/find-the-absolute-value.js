1285bdecbc3d7f5ba9792dc91f3f32cf2eeef5e7
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-05-20 12:49:16 +0900

    Title: 절댓값으로 변경, Time: 187ms, Memory: 14MB, Status: Passed - CodeTree

--- 240520/절댓값으로 변경/find-the-absolute-value.js
+++ 240520/절댓값으로 변경/find-the-absolute-value.js
@@ -5,6 +5,8 @@ const arr = input[1].split(' ').map(Number);
 
 function makeAbs(arr) {
     let result = arr.map(el => Math.abs(el));
+
+    return result;
 }
 
 let result = makeAbs(arr).join(' ');
