f6e9ae1e45b2dfd306b2a51e6c15c585123a68cd
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-05-20 12:44:24 +0900

    Title: 두 정수에 대한 연산값, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240520/두 정수에 대한 연산값/operational-values-for-two-integers.js
+++ 240520/두 정수에 대한 연산값/operational-values-for-two-integers.js
@@ -4,8 +4,8 @@ const [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 // 큰 수 + 25, 작은 수 * 2
 
 function mutate(a, b) {
-    const bigger = Math.Max(a, b);
-    const smaller = Math.Min(a, b);
+    const bigger = Math.max(a, b);
+    const smaller = Math.min(a, b);
 
     return [bigger + 25, smaller * 2];
 }
