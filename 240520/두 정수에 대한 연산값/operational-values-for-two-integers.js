6fcd921dfc9606951d239634dda33dfaee9ac2d3
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-05-20 12:43:26 +0900

    Title: 두 정수에 대한 연산값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240520/두 정수에 대한 연산값/operational-values-for-two-integers.js
+++ 240520/두 정수에 대한 연산값/operational-values-for-two-integers.js
@@ -4,8 +4,8 @@ const [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 // 큰 수 + 25, 작은 수 * 2
 
 function mutate(a, b) {
-    const bigger = math.Max(a, b);
-    const smaller = math.Min(a, b);
+    const bigger = Math.Max(a, b);
+    const smaller = Math.Min(a, b);
 
     return [bigger + 25, smaller * 2];
 }
