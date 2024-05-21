545bae4c22e25f812ac782f5f0be8ba1e28cac5e
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-05-21 09:53:14 +0900

    Title: 두 정수에 대한 연산값 2, Time: 222ms, Memory: 47MB, Status: Wrong Answer - CodeTree

---
+++ 240521/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+
+const mutate = (arr) => {
+    let [a, b] = arr;
+    if (a > b) {
+        a *= 2;
+        b += 10;
+    } else {
+        a += 10;
+        b *= 2;
+    }
+
+    return [a ,b];
+}
+
+console.log(mutate(input));
