c7d9b7d8d9f9c57295ffba2ca9f2428d16d417be
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-06-04 21:39:30 +0900

    Title: 1부터 특정 수까지의 합 2, Time: 319ms, Memory: 14MB, Status: Passed - CodeTree

--- 240604/1부터 특정 수까지의 합 2/sum-from-1-to-a-certain-number-2.js
+++ 240604/1부터 특정 수까지의 합 2/sum-from-1-to-a-certain-number-2.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 let input = Number(fs.readFileSync(0).toString().trim());
 
-const factorial = (n) => {
+const getSum = (n) => {
     if (n === 1) {
         return 1;
     }
     
-    return n * factorial(n - 1);
+    return n + getSum(n - 1);
 }
 
-console.log(factorial(input));
+console.log(getSum(input));
