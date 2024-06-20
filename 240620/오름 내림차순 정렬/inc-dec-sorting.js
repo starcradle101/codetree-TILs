e1077e848ec0c77ad9d1e0b85d2bd4dfccf38bb6
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-06-20 17:05:11 +0900

    Title: 오름 내림차순 정렬, Time: 240ms, Memory: 14MB, Status: Passed - CodeTree

--- 240620/오름 내림차순 정렬/inc-dec-sorting.js
+++ 240620/오름 내림차순 정렬/inc-dec-sorting.js
@@ -3,5 +3,7 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const arr = input[1].split(' ').map(Number);
 
-console.log(arr.sort((a, b) => a - b));
-console.log(arr.sort((a,b) => b-a));
+const ascend = arr.sort((a,b) => a - b).join(' ');
+const descend = arr.sort((a,b) => b - a).join(' ');
+
+console.log(`${ascend}\n${descend}`);
