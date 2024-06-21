9985da84e559759e3dad4457b5d32af44a705f0f
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-06-21 16:04:37 +0900

    Title: 중앙값 계산 2, Time: 189ms, Memory: 14MB, Status: Passed - CodeTree

--- 240621/중앙값 계산 2/get-median-2.js
+++ 240621/중앙값 계산 2/get-median-2.js
@@ -7,8 +7,8 @@ let answer = [arr[0]];
 for (let i = 1; i < arr.length; i += 1) {
     // 홀수 번째니까 짝수 인덱스
     if (i % 2 === 0) {
-        const subArr = arr.slice(0, i + 1);
-        answer.push(subArr[Math.floor(i/2)]);
+        const sortedSubArr = arr.slice(0, i + 1).sort((a, b) => a - b);
+        answer.push(sortedSubArr[Math.floor(i/2)]);
     }
 }
 
