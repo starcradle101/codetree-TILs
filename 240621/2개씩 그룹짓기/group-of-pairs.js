cfd3d21a68ee3d7d7338daae97c96cc2af2ca6bd
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-06-21 15:30:04 +0900

    Title: 2개씩 그룹짓기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240621/2개씩 그룹짓기/group-of-pairs.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const arr = input[1].split(' ').map(Number)
+
+const f = (arr) => {
+    
+}
+
+console.log(f(arr));
+
+8 2 7 1 3 5
+
+1 2 3 5 7 8
