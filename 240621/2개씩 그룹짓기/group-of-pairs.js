88a2a8ee1c46ece8aae9f13710642c6fbdfc9f83
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-06-21 15:39:26 +0900

    Title: 2개씩 그룹짓기, Time: 184ms, Memory: 14MB, Status: Passed - CodeTree

--- 240621/2개씩 그룹짓기/group-of-pairs.js
+++ 240621/2개씩 그룹짓기/group-of-pairs.js
@@ -1,13 +1,21 @@
-const fs = require('fs');
+const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
-const arr = input[1].split(' ').map(Number)
 
-const f = (arr) => {
-    
+// 변수 선언 및 입력
+const n = Number(input[0]);
+const nums = input[1].trim().split(' ').map(Number);
+
+// nums를 정렬합니다.
+nums.sort((a, b) => a - b);
+
+let group_max = 0;
+for (let i = 0; i < n; i++) {
+    // i번째와 2n - 1 - i번째 원소를 매칭합니다.
+    const group_sum = nums[i] + nums[2 * n - 1 - i];
+    if (group_sum > group_max) {
+        // 최댓값을 갱신합니다.
+        group_max = group_sum;
+    }
 }
 
-console.log(f(arr));
-
-8 2 7 1 3 5
-
-1 2 3 5 7 8
+console.log(group_max);
