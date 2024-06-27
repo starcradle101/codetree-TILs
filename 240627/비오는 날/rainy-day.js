9c7a97a7b9cf420387c6a3094ad2d2ee4a905d89
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-06-27 14:56:09 +0900

    Title: 비오는 날, Time: 195ms, Memory: 14MB, Status: Passed - CodeTree

--- 240627/비오는 날/rainy-day.js
+++ 240627/비오는 날/rainy-day.js
@@ -10,9 +10,19 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const weatherData = input.slice(1).map(el => new WeatherData(...el.split(' ')));
 
+let rainyDays = [];
+
 for (let datum of weatherData) {
-    if (datum.weather === 'Rain') {
-        console.log(`${datum.date} ${datum.day} ${datum.weather}`);
-        break;
-    }
+    if (datum.weather === 'Rain') rainyDays.push(datum);
 }
+
+let earliestRainyDay = rainyDays[0];
+
+for (let rainyDay of rainyDays) {
+    if (rainyDay.date < earliestRainyDay.date) earliestRainyDay = rainyDay;
+}
+
+console.log(`${earliestRainyDay.date} ${earliestRainyDay.day} ${earliestRainyDay.weather}`);
+
+// 가장 근 시일 내에
+// 비가 오는 날을 찾아 출력
