3b8a3c89996375466915a67816f1519da61ab339
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-07-09 21:54:30 +0900

    Title: 요일 맞추기, Time: 170ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240709/요일 맞추기/guess-day-of-week.js
+++ 240709/요일 맞추기/guess-day-of-week.js
@@ -4,32 +4,44 @@ const num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 const dates = ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'];
 
 const getDaysInterval = (m1, d1, m2, d2) => {
-    const minusFlag = (m1 >= m2 && d1 > d2) || (m1 > m2) ? true : false;
+    const isNegative = m1 > m2 || (m1 === m2 && d1 > d2) ? true : false;
     let totalDays = 0;
-    const [mBefore, mAfter] = [m1, m2].sort((a,b) => a - b);
-
-    for (let i = mBefore; i < mAfter; i += 1) {
-        totalDays += num_of_days[i];
+    if (m1 > m2) {
+        for (let i = m2; i < m1; i += 1) {
+            totalDays += num_of_days[i];
+        }
     }
-    
-    totalDays += Math.abs(d1 - d2)
-    return [totalDays, minusFlag];
+
+    if (m1 < m2) {
+        for (let i = m1; i < m2; i += 1) {
+            totalDays += num_of_days[i];
+        }
+    }
+
+    totalDays += Math.abs(d1 - d2);
+    return [totalDays, isNegative];
 }
 
 const getDate = (m1, d1, m2, d2) => {
-    const [totalDays, minusFlag] = getDaysInterval(m1, d1, m2, d2);
-    if (minusFlag) {
-        const date = getNegativeIndex(dates, -1 * (totalDays % 7));
-        return date;
-    }
-
-    const date = dates[totalDays%7]
+    const [totalDays, isNegative] = getDaysInterval(m1, d1, m2, d2);
+    const date = getIndex(totalDays, isNegative)
     return date;
 }
 
-const getNegativeIndex = (array, negativeIndex) => {
-    const index = array.length + negativeIndex;
-    return array[index];
+const getIndex = (totalDays, isNegative) => {
+    const index = isNegative ?  dates.length + -1 * (totalDays % 7) : dates.length + totalDays % 7;
+    return dates[index];
 }
 
 console.log(getDate(m1, d1, m2, d2));
+
+// 다시 생각해보자
+
+// m1 > m2
+// 음수인지 알고 월에 따른 일수 - d2 + d1
+
+// m1 < m2
+// 월에 따른 일수 - d1 + d2
+
+// m1 === m2
+// 음수인지 알고, | d1 - d2 |
