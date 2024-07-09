35fce07083d3b1c332063fec14dd8984026988a7
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2024-07-09 21:57:40 +0900

    Title: 요일 맞추기, Time: 163ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240709/요일 맞추기/guess-day-of-week.js
+++ 240709/요일 맞추기/guess-day-of-week.js
@@ -29,9 +29,11 @@ const getDate = (m1, d1, m2, d2) => {
 }
 
 const getIndex = (totalDays, isNegative) => {
-    console.log(totalDays % 7, isNegative)
+    if (totalDays === 0) {
+        return dates[0];
+    }
+
     const index = isNegative ?  dates.length + -1 * (totalDays % 7) : dates.length + totalDays % 7;
-    console.log(index)
     return dates[index];
 }
 
