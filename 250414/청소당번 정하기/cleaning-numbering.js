378156f2a9f43741507139a52521a710ba68428e
starcradle101 <113353436+starcradle101@users.noreply.github.com>
2025-04-14 23:55:02 +0900

    Title: 청소당번 정하기, Time: 174ms, Memory: 15MB, Status: Passed - Codetree

--- 250414/청소당번 정하기/cleaning-numbering.js
+++ 250414/청소당번 정하기/cleaning-numbering.js
@@ -9,14 +9,14 @@ let classroom_cnt = 0;
 let corridor_cnt = 0;
 let toilet_cnt = 0;
 
-for(let i = 0; i <= input; i+= 1) {
-    if (i % 12 === 0) {
+for(let i = 1; i <= input; i+= 1) {
+    if (i % 12 === 0 && i!==1) {
     toilet_cnt++;
     continue;
-  } else if (i % 3 === 0) {
+  } else if (i % 3 === 0 && i!==1) {
     corridor_cnt++;
     continue;
-  } else if (i % 2 === 0) {
+  } else if (i % 2 === 0 && i!==1) {
     classroom_cnt++;
     continue;
   }
