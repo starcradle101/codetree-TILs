b34ec68690aece735180fb91ba56551c0d549bfb
starcradle101 <113353436+starcradle101@users.noreply.github.com>
2025-04-14 23:44:18 +0900

    Title: 배수의 개수, Time: 167ms, Memory: 15MB, Status: Passed - Codetree

--- 250414/배수의 개수/number-of-multipliers.js
+++ 250414/배수의 개수/number-of-multipliers.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split('\n').map(Number);
 
-let cnt = 0;
+let cnt_3 = 0;
+let cnt_5 = 0;
 
 for(let num of input) {
-    if(num % 3 === 0 || num % 5 === 0) cnt += 1;
+    if(num % 3 === 0) cnt_3 += 1;
+    if(num % 5 === 0) cnt_5 += 1;
 }
 
-console.log(cnt)
+console.log(cnt_3, cnt_5)
