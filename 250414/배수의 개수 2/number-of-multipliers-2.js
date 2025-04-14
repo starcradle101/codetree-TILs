9671d20cb0550dbb13a27a8a321e378b40e9c4ae
starcradle101 <113353436+starcradle101@users.noreply.github.com>
2025-04-14 23:40:41 +0900

    Title: 배수의 개수 2, Time: 185ms, Memory: 15MB, Status: Passed - Codetree

--- 250414/배수의 개수 2/number-of-multipliers-2.js
+++ 250414/배수의 개수 2/number-of-multipliers-2.js
@@ -4,7 +4,7 @@ let input = fs.readFileSync(0).toString().trim().split('\n').map(Number);
 let cnt = 0;
 
 for (let el of input) {
-    if(el % 2 === 1) answer += 1;
+    if(el % 2 === 1) cnt += 1;
 }
 
 console.log(cnt)
