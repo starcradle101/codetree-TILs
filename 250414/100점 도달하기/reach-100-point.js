7494de0f0aae4db19b5fc7d1382689da0bc1f41f
starcradle101 <113353436+starcradle101@users.noreply.github.com>
2025-04-14 23:22:32 +0900

    Title: 100점 도달하기, Time: 180ms, Memory: 15MB, Status: Passed - Codetree

--- 250414/100점 도달하기/reach-100-point.js
+++ 250414/100점 도달하기/reach-100-point.js
@@ -4,13 +4,13 @@ let score = Number(fs.readFileSync(0).toString().trim());
 let answer = '';
 
 for(let i = score; i <= 100; i += 1) {
-    if(score >= 90) {
+    if(i >= 90) {
         answer += 'A ';
-    } else if(score >= 80) {
+    } else if(i >= 80) {
         answer += 'B ';
-    } else if(score >= 70) {
+    } else if(i >= 70) {
         answer += 'C ';
-    } else if(score >= 60) {
+    } else if(i >= 60) {
         answer += 'D ';
     } else {
         answer += 'F ';
