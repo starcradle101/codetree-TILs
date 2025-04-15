54a216be6c577713fabe5cdd7fdb96810cc8eec5
starcradle101 <113353436+starcradle101@users.noreply.github.com>
2025-04-15 09:52:03 +0900

    Title: 입력받은 수의 합과 평균, Time: 186ms, Memory: 15MB, Status: Passed - Codetree

--- 250415/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.js
+++ 250415/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.js
@@ -1,10 +1,11 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n').map(Number);
+const arr = input.slice(1);
 
 let sum = 0;
-const cnt = input.length;
+const cnt = input.length - 1;
 
-for(let num of input) {
+for(let num of arr) {
     sum += num;
 }
 
