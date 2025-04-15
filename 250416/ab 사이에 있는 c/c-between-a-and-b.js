1f5f2ee5bf2daf5d23985f00dcb553f0729ca3fc
starcradle101 <113353436+starcradle101@users.noreply.github.com>
2025-04-16 01:57:34 +0900

    Title: ab 사이에 있는 c, Time: 171ms, Memory: 15MB, Status: Passed - Codetree

--- 250416/ab 사이에 있는 c/c-between-a-and-b.js
+++ 250416/ab 사이에 있는 c/c-between-a-and-b.js
@@ -4,7 +4,7 @@ const [a, b, c] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 let answer = false;
 
 for(let i = a; i <= b; i += 1) {
-    if(i % c === 0) answer = !answer;
+    if(i % c === 0) answer = true;
 }
 
 console.log(answer ? 'YES' : 'NO')
