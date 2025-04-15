883188434f369a59154322f18aacd99812e2be30
starcradle101 <113353436+starcradle101@users.noreply.github.com>
2025-04-15 20:12:47 +0900

    Title: 1부터의 합, Time: 176ms, Memory: 15MB, Status: Passed - Codetree

--- 250415/1부터의 합/sum-from-the-one.js
+++ 250415/1부터의 합/sum-from-the-one.js
@@ -4,9 +4,9 @@ const n = Number(fs.readFileSync(0).toString().trim());
 let sum = 0;
 
 for(let i = 1; i <= 100; i += 1) {
+    sum += i;
     if(sum >= n) {
         console.log(i);
         break;
     }
-    sum += i;
 }
