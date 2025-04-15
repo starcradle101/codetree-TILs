26aa9122ea0764fa05116087669c8b54d4d62749
starcradle101 <113353436+starcradle101@users.noreply.github.com>
2025-04-16 03:06:44 +0900

    Title: 별표 출력하기 9, Time: 187ms, Memory: 15MB, Status: Passed - Codetree

--- 250416/별표 출력하기 9/print-star-9.js
+++ 250416/별표 출력하기 9/print-star-9.js
@@ -8,16 +8,9 @@ for(let i = 0; i < n; i += 1) {
         line += '  ';
     }
 
-    for(let k = 1; k < 2 * i + 1; k += 1) {
+    for(let k = 0; k < 2 * i + 1; k += 1) {
         line += '* ';
     }
     
     console.log(line)
 }
-
-// 공백은 언제나 항상 n - 1개였음
-
-// n = 5 4개
-// 별은 2n + 1개 
-
-// 1부터 2i + 1 => 2i개
