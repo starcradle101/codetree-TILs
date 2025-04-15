2719f28264695cdc4a4c09fc17704c44b7a5df2f
starcradle101 <113353436+starcradle101@users.noreply.github.com>
2025-04-15 15:40:46 +0900

    Title: A의 B승, Time: 174ms, Memory: 15MB, Status: Passed - Codetree

--- 250415/A의 B승/a-to-the-power-of-b.js
+++ 250415/A의 B승/a-to-the-power-of-b.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
-let prod = a;
+let prod = 1;
 
 for(let i = 1; i <= b; i += 1) {
     prod *= a;
