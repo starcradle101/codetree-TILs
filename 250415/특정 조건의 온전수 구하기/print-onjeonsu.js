1dffc348e8c251ef9f10b07de93a035edb2e0cbd
starcradle101 <113353436+starcradle101@users.noreply.github.com>
2025-04-15 20:07:22 +0900

    Title: 특정 조건의 온전수 구하기, Time: 177ms, Memory: 15MB, Status: Passed - Codetree

--- 250415/특정 조건의 온전수 구하기/print-onjeonsu.js
+++ 250415/특정 조건의 온전수 구하기/print-onjeonsu.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 const n = Number(fs.readFileSync(0).toString().trim());
 
-let answer = 0;
+let answer = '';
 
 for(let i = 1; i <= n; i += 1) {
     if(i % 2 === 0 || i % 10 === 5 || (i % 3 === 0 && i % 9 !== 0)) continue;
-    answer += 1;
+    answer += i + ' ';
 }
 
 console.log(answer);
