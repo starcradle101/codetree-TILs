32515c4300e80cc7f105347f13b8da2ecf42ead4
starcradle101 <113353436+starcradle101@users.noreply.github.com>
2025-04-16 02:27:06 +0900

    Title: 별표 출력하기, Time: 181ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250416/별표 출력하기/print-star.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const n = Number(fs.readFileSync(0).toString().trim());
+
+let str = '';
+let cnt = 1;
+
+for(let i = 1; i < n * 2; i += 1) {
+    str = '';
+    for(let j = 0; j < cnt; j += 1) {
+        str += '* ';
+    }
+    console.log(str);
+
+    if(i < n) {
+        cnt += 1;
+    } else {
+        cnt -= 1;
+    }
+}
