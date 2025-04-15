7a014d94626479e9b14899c251e82e6448c1ef3d
starcradle101 <113353436+starcradle101@users.noreply.github.com>
2025-04-15 21:30:27 +0900

    Title: 사각형의 넓이 계속 계산하기, Time: 158ms, Memory: 14MB, Status: Runtime Error - Codetree

---
+++ 250415/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split('\n').map(Number);
+
+let idx = 0;
+
+while(true) {
+    const line = input[idx].split(' ');
+    const [width, height] = line.splice(0, 2).map(Number);
+    const area = width * height;
+    
+    console.log(area);
+    
+    idx += 1;
+
+    if(line[2] === 'C') break;
+}
