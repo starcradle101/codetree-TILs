d32cf9a65f09bcacb5bf1fdb2be3f6b9ec714d22
starcradle101 <113353436+starcradle101@users.noreply.github.com>
2025-04-18 08:43:28 +0900

    Title: 별 그리기, Time: 188ms, Memory: 15MB, Status: Passed - Codetree

--- 250418/별 그리기/star-drawing.js
+++ 250418/별 그리기/star-drawing.js
@@ -1,24 +1,25 @@
 const fs = require("fs");
 const n = Number(fs.readFileSync(0).toString().trim());
 
-let str_cnt = 2 * n - 1;
-let empty_cnt = 0;
+let str_cnt = 1;
+let empty_cnt = n - 1;
 
 for(let i = 1; i <= 2 * n - 1; i += 1) {
     let line = '';
-    for(let j = 0; j < empty_cnt; j += 1) {
-        line += ' *';
+    
+    for(let k = 0; k < empty_cnt; k += 1) {
+        line += ' ';
     }
-    for(let k = 0; k < str_cnt; k += 1) {
-        line += '* ';
+    for(let j = 0; j < str_cnt; j += 1) {
+        line += '*';
     }
 
-    if(i < n) {
-        str_cnt += 2;
-        empty_cnt -= 1;
-    } else {
+    if(i >= n) {
         str_cnt -= 2;
         empty_cnt += 1;
+    } else {
+        str_cnt += 2;
+        empty_cnt -= 1;
     }
 
     console.log(line)
