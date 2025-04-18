f048e1682b65a5d5a842928aad376245e1ce7b11
starcradle101 <113353436+starcradle101@users.noreply.github.com>
2025-04-18 09:26:03 +0900

    Title: 별표 출력하기 10, Time: 181ms, Memory: 15MB, Status: Passed - Codetree

--- 250418/별표 출력하기 10/print-star-10.js
+++ 250418/별표 출력하기 10/print-star-10.js
@@ -1,35 +1,25 @@
 const fs = require("fs");
 const n = Number(fs.readFileSync(0).toString().trim());
 
-for(let i = 0; i < n; i += 1) {
+let odd_str = n;
+let even_str = 1;
+
+for(let i = 0; i < 2 * n; i += 1) {
     let line = '';
 
     if(i % 2 === 0) {
-        line += '*'
-    } else {
-        for(let j = 0; j < i + 1; j += 1) {
+        for(let j = 0; j < even_str; j += 1) {
             line += '* ';
         }
+
+       even_str += 1;
+    } else {
+        for(let j = odd_str; j > 0; j -= 1) {
+            line += '* ';
+        }
+
+        odd_str -= 1;
     }
 
     console.log(line)
 }
-
-// 인덱스 짝수는 1, 2, 3...0 < n - 2 까지 1씩 증가
-// 인덱스 홀수는 5, 4, 3... n > n-1까지 1씩 감소
-
-// n = 3
-// 0 1개
-// 1 3개
-// 2 2개
-// 3 2개
-// 4 3개
-// 5 1개
-
-// n = 4
-// 0 1개
-// 1 1개
-// 2 1개
-// 3 1개
-// 4 1개
-// 5 1개
