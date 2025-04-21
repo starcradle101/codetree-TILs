7ed5c1f57da46d2ce81617cd69c63a01d48fad14
starcradle101 <113353436+starcradle101@users.noreply.github.com>
2025-04-21 23:41:10 +0900

    Title: 왔다 갔던 구역 2, Time: 182ms, Memory: 15MB, Status: Passed - Codetree

--- 250421/왔다 갔던 구역 2/area-been-to-and-from2.js
+++ 250421/왔다 갔던 구역 2/area-been-to-and-from2.js
@@ -3,32 +3,32 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input[0]);
 const commands = input.slice(1, n + 1);
-let answer = 0;
 
 const line = new Array(201).fill(0);
 let start = 100;
 
-for(let command of commands) {
-    let [x, direction] = command.split(' ');
-    x = Number(x);
+for (let command of commands) {
+  let [x, direction] = command.split(' ');
+  x = Number(x);
 
-    if(direction === 'L') {
-        for(let i = start; i <= start + x; i += 1) {
-            line[i] += 1;
-        }
-        start -= x;
-    } else {
-        for(let i = start + x; i >= start; i -= 1) {
-            line[i] += 1;
-        }
-        start += x;
+  if (direction === 'L') {
+    for (let i = start - 1; i >= start - x; i--) {
+      line[i] += 1;
     }
+    start -= x;
+  } else {
+    for (let i = start; i < start + x; i++) {
+      line[i] += 1;
+    }
+    start += x;
+  }
 }
 
-for(let i = 0; i < line.length - 1; i += 1) {
-    if(line[i] >= 2 && line[i + 1] >= 2) {
-        answer += 1;
-    }
+let answer = 0;
+for (let i = 0; i < line.length; i++) {
+  if (line[i] >= 2) {
+    answer += 1;
+  }
 }
 
-console.log(answer)
+console.log(answer);
