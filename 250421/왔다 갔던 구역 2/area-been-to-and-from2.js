20bea0e0156268ef1a0bada5f9d285aec59e76d4
starcradle101 <113353436+starcradle101@users.noreply.github.com>
2025-04-21 23:12:29 +0900

    Title: 왔다 갔던 구역 2, Time: 173ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250421/왔다 갔던 구역 2/area-been-to-and-from2.js
+++ 250421/왔다 갔던 구역 2/area-been-to-and-from2.js
@@ -29,13 +29,4 @@ for(let i = 0; i < line.length - 1; i += 1) {
     }
 }
 
-
-
-// Please Write your code here.
-
-// 2번 이상 지나간 영역의 크기를 출력 
-// xL 왼쪽으로 x
-// xR 오른쪽으로 x
-
-// 케이스를 바탕으로 계산 -> 길이 201인 배열, 시작 인덱스는 0
-// 음수일떄와 양수일 때 반복문이 다름
+console.log(answer)
